6d9897c98797fccabeec030e9156656392b8e35e
kohumukini <kealii.ohumukini@gmail.com>
2026-06-01 11:49:39 -0700

    Implemented all three main methods

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,22 @@
 public class TreeNode {
+    public int value; 
+    public TreeNode left; 
+    public TreeNode right; 
 
-  
+    // TreeNode myNode = new TreeNode(8, null, someOtherNode); 
+
+    public TreeNode(int value, TreeNode left, TreeNode right) {
+        this.value = value; 
+        this.left = left;
+        this.right = right; 
+    }
+
+    public TreeNode(int value) {
+        this.value = value; 
+    }
+
+    public TreeNode(int value, TreeNode left) {
+        this.value = value;
+        this.left = left; 
+    }
 }
