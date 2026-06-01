290d166d754712b71279560a99674b0d8763f5e9
Un01 <274392693+alexanderpena014-svg@users.noreply.github.com>
2026-06-01 11:51:10 -0700

    in class work

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,27 @@
 public class TreeNode {
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
 
+    // TreeNode myNode = new TreeNode(8, null, someOtherNode);
   
+    public TreeNode(int value, TreeNode left, TreeNode right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
+
+    // public TreeNode(int value, TreeNode left) {
+    //     this.value = value;
+    //     this.left = left;
+    // }
+
+    // public TreeNode(int value, TreeNode right) {
+    //     this.value = value;
+    //     this.left = right;
+    // }
 }
