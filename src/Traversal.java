aab7335deb6ee0320c6e0b7afb9887a456841e94
Elijah_Rhodes <mastergamer3dr@gmail.com>
2026-06-01 11:51:13 -0700

    End of class - Completed Program

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,5 +1,46 @@
 public class Traversal {
   public static void main(String[] args) {
+    TreeNode root = new TreeNode(10);
+    
+    root.left = new TreeNode(9);
+    root.left.left = new TreeNode(5);
+    root.left.right = new TreeNode(2);
 
+    root.right = new TreeNode(15, new TreeNode(-3), new TreeNode(5, null, new TreeNode(22)));
+    
+    //root.right.left = new TreeNode(-3);
+    //System.out.println(root.right.right.right.data);
+
+    preorder(root);
+    System.out.println();
+
+    postorder(root);
+    System.out.println();
+
+    inorder(root);
+  }
+
+  public static void preorder(TreeNode current)
+  {
+    if(current == null) return;
+    System.out.println(current.data);
+    preorder(current.left);
+    preorder(current.right);
+  }
+
+  public static void postorder(TreeNode current)
+  {
+    if(current == null) return;
+    postorder(current.left);
+    postorder(current.right);
+    System.out.println(current.data);
+  }
+
+  public static void inorder(TreeNode current)
+  {
+    if(current == null) return;
+    inorder(current.left);
+    System.out.println(current.data);
+    inorder(current.right);
   }
 }
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,31 @@
 public class TreeNode {
+    public int data;
+    public TreeNode left;
+    public TreeNode right;
 
-  
+    public TreeNode(int data, TreeNode left, TreeNode right)
+    {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    public TreeNode(int data)
+    {
+        this.data = data;
+    }
+
+    /*
+    public TreeNode(TreeNode left, int data)
+    {
+        this.data = data;
+        this.left = left;
+    }
+
+    public TreeNode(int data, TreeNode right)
+    {
+        this.data = data;
+        this.right = right;
+    }
+    */
 }
