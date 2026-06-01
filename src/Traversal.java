edb4cf274b97c62cd44a4858919ee58d584b28d0
alex <85966686+AlexandrBalan@users.noreply.github.com>
2026-06-01 11:43:31 -0700

    this is live coded during class, implemented the preOrder and postOrder.

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,5 +1,29 @@
 public class Traversal {
   public static void main(String[] args) {
+    TreeNode root = new TreeNode(10);
 
+    root.left = new TreeNode(9);
+    root.left.left = new TreeNode(5);
+    root.left.right = new TreeNode(2);
+
+    root.right = new TreeNode(15, new TreeNode(-3), new TreeNode(5, null, new TreeNode(22)));
+
+    preOrder(root);
+  }
+
+  public static void preOrder(TreeNode current) {
+    if (current == null) return;
+
+    System.out.println(current.value);
+    preOrder(current.left);
+    preOrder(current.right);
+  }
+
+  public static void postOrder(TreeNode current) {
+    if (current == null) return;
+
+    postOrder(current.left);
+    postOrder(current.right);
+    System.out.println(current.value);
   }
 }
