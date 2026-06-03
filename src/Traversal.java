e722200423c09721095be892f9836f8b85badc34
annaliese_scott <274317330+annaliesescott@users.noreply.github.com>
2026-06-03 11:33:47 -0700

    Followed along for day two of class instruction

--- src/Traversal.java
+++ src/Traversal.java
@@ -10,21 +10,40 @@ public class Traversal {
     //right subtree
     root.right = new TreeNode(15, new TreeNode(-3), new TreeNode(5, null, new TreeNode(22)));
 
-    preorder(root);
+    /*preorder(root);
     System.out.println();
     postorder(root);
     System.out.println();
-    inorder(root);
+    inorder(root);*/
+
+    printUnderThreshold(root, 11);
+    System.out.println(countNodes(root));
+  }
+
+  //print ALL nodes in the tree that have data that is less than the threshold
+  public static void printUnderThreshold(TreeNode current, int threshold){
+    if(current == null) return;
+    if(current.value<threshold){
+      System.out.println(current.value);
+    }
+    printUnderThreshold(current.left, threshold);
+    printUnderThreshold(current.right, threshold);
+  }
+
+  public static int countNodes(TreeNode current){
+    if(current == null) return 0;
+    //count left subtree
+    int leftCount = countNodes(current.left);
+    //count right subtree
+    int rightCount = countNodes(current.right);
+    //add one for the root
+    return leftCount + rightCount + 1;
   }
 
   public static void preorder(TreeNode current){
     if(current == null) return;
     System.out.print(current.value + " ");
-
-    //explore left
     preorder(current.left);
-
-    //explore right
     preorder(current.right);
 
   }
