ce7bba6e18d4747637da748f1d8c76c9addd1ab2
Mo <274354989+MoTheTurtle@users.noreply.github.com>
2026-06-03 11:10:03 -0700

    Day 2

--- src/Traversal.java
+++ src/Traversal.java
@@ -13,9 +13,32 @@ public class Traversal {
                                             
     //preorder(root);
     //postOrder(root);
-    inOrder(root);
-
+    //inOrder(root);
+    printUnderThreshold(root, 5);
     
+  }
+
+public static int countNodes(TreeNode current){
+  //count left subtree
+if(current== null) return 0; 
+int leftCount = countNodes(current.left);
+//count right subtree
+int rightCount = countNodes(current.right);
+//add one for the root
+int totalCount = leftCount + rightCount + 1;
+return totalCount;
+
+}
+// Print All nodes the tree that has data strictly less the threshold
+//nodes should be printed pre-order
+  public static void printUnderThreshold(TreeNode current, int threshold){
+     if(current == null) return;
+     if(current.value < threshold){
+    System.out.println(current.value);
+     }
+    printUnderThreshold(current.left,threshold);
+    printUnderThreshold(current.right,threshold);
+    return;
   }
 
   public static void preorder(TreeNode current){
